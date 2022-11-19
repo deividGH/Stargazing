00a1ec1c8ffe7ed327b538c809cba8cc5c50d4d7
deividGH <deivid.toloza@gmail.com>
2022-11-19 18:49:05 -0500

    formularosky

--- src/Modales.jsx
+++ src/Modales.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import rana from "./img/ranafinal.jpg";
 
 const Modales = () => {
+
+  const alform = () =>{
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSemNMPDqaihac6HKzLW_HbCeFBqqtt0E-cLD7_xVUzYnfgX-A/viewform?usp=sf_link'
+  }
   return (
     <center>
       <div className="container">
@@ -10,14 +14,40 @@ const Modales = () => {
           <p>Modal verbs</p>
         </div>
         <div className="def">
+          <p className="superp">Definition</p>
           <p>
             Modal verbs are auxiliary verbs that we use to express an opinion
             about whether something is probable or possible. We also use them
             when talking about ability, asking permission, or making a request.
           </p>
-          <p>Modal verbs list:</p>
-          <p>Can, May, Will, Ought to, Must, Could, Might</p>
-          <p>Shall, Should do, Would</p>
+          <div>
+            <p className="underline">Interrogative</p>
+            <p className="font16">
+              When we want to ask something and there is a modal verb in the
+              sentence, we must put the modal at the beginning of the sentence,
+              followed by the subject and the main verb.
+            </p>
+          </div>
+          <div className="structures">
+            <p>
+              <span className="rojo">Modal verb</span>+
+              <span className="verde">Subject</span>+
+              <span className="morado">Principal verb</span>+
+              <span className="azul">Complement</span>
+            </p>
+            <p>
+              <span className="rojo2">Would</span>{" "}
+              <span className="verde2">you</span>{" "}
+              <span className="morado2">dance</span>{" "}
+              <span className="azul2">with me?</span>{" "}
+            </p>
+            <p>(¿Bailarías conmigo?)</p>
+          </div>
+          <div>
+            <p>Modal verbs list:</p>
+            <p>Can, May, Will, Ought to, Must, Could, Might</p>
+            <p>Shall, Should do, Would</p>
+          </div>
         </div>
         <div className="estructura">
           <div>
@@ -68,33 +98,10 @@ const Modales = () => {
             </p>
             <p>(Él no puede levantar tanto peso)</p>
           </div>
-          <div>
-            <p className="underline">Interrogative</p>
-            <p>
-              When we want to ask something and there is a modal verb in the
-              sentence, we must put the modal at the beginning of the sentence,
-              followed by the subject and the main verb.
-            </p>
-          </div>
-          <div className="structures">
-            <p>
-              <span className="rojo">Modal verb</span>+
-              <span className="verde">Subject</span>+
-              <span className="morado">Principal verb</span>+
-              <span className="azul">Complement</span>
-            </p>
-            <p>
-              <span className="rojo2">Would</span>{" "}
-              <span className="verde2">you</span>{" "}
-              <span className="morado2">dance</span>{" "}
-              <span className="azul2">with me?</span>{" "}
-            </p>
-            <p>(¿Bailarías conmigo?)</p>
-          </div>
         </div>
       </div>
       <div className="siguiente">
-        <input type="button" value="CONTINUE" className="button2" />
+        <input type="button" value="CONTINUE" className="button2"  onClick={alform}/>
       </div>
     </center>
   );
